a11fdbe04ec21e4904d2a4f600877ce9a1898224
Nicholas L <NLoo1@outlook.com>
2024-09-22 02:13:37 -0400

    Update config

--- config/config.js
+++ config/config.js
@@ -2,31 +2,34 @@
 
 /** Shared config for application; can be required many places. */
 
-const dotenvConfig = { path: process.env.NODE_ENV ? ".env." + process.env.NODE_ENV : ".env" }
-require("dotenv").config(dotenvConfig)
+const dotenvConfig = { path: process.env.NODE_ENV ? ".env." + process.env.NODE_ENV : ".env" };
+require("dotenv").config(dotenvConfig);
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD 
-const DB_NAME = process.env.NODE_ENV === "test" ? "pokeapi_test" : "pokeapi";
-const PORT = +process.env.PORT || 3001;
+// Use DATABASE_URL for production; fallback to individual settings for local development/testing
+const DATABASE_URL = process.env.DATABASE_URL || {
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.NODE_ENV === "test" ? "pokeapi_test" : "pokeapi",
+  host: 'localhost', // Use 'localhost' for local development
+  port: 5432, // PostgreSQL default port
+};
 
+const PORT = +process.env.PORT || 3001;
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 // console.log("Poke-API Config:".green);
 // console.log("SECRET_KEY:".yellow, SECRET_KEY);
 // console.log("PORT:".yellow, PORT.toString());
 // console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-// console.log("Database:".yellow, DB_NAME);
+// console.log("Database:".yellow, DATABASE_URL);
 // console.log("---");
 
 module.exports = {
   SECRET_KEY,
   PORT,
   BCRYPT_WORK_FACTOR,
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_NAME
+  DATABASE_URL,
 };
